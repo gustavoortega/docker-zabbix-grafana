61640a5d13b47806a0eb117508dde2b1e9e95a5b
Gustavo Ortega <gustavo.ortega@ingenia.la>
2021-04-23 20:57:27 -0300

    agregados para discovery en red

--- deploy.sh
+++ deploy.sh
@@ -32,10 +32,18 @@ docker run --name zabbix-server -t \
       -e DB_SERVER_HOST="mysql-server" \
       -e MYSQL_DATABASE="zabbix" \
       -e MYSQL_USER="zabbix" \
-      -e MYSQL_PASSWORD="zabbix_pwd" \
+      -e MYSQL_PASSWORD="$mysql_password" \
       -e ZBX_CACHESIZE=256M \
       -e ZBX_STARTVMWARECOLLECTORS=2 \
       -e ZBX_TIMEOUT=20 \
+      -e ZBX_STARTPOLLERS=5       \
+      -e ZBX_IPMIPOLLERS=1       \
+      -e ZBX_STARTPREPROCESSORS=3        \
+      -e ZBX_STARTPOLLERSUNREACHABLE=1       \
+      -e ZBX_STARTTRAPPERS=5       \
+      -e ZBX_STARTPINGERS=5       \
+      -e ZBX_STARTDISCOVERERS=5       \
+      -e ZBX_STARTHTTPPOLLERS=5       \
       --network zabbix-net \
       --link mysql-server:mysql \
       -p 10051:10051 \
@@ -47,7 +55,7 @@ docker run --name zabbix-web -t \
       -e DB_SERVER_HOST="mysql-server" \
       -e MYSQL_DATABASE="zabbix" \
       -e MYSQL_USER="zabbix" \
-      -e MYSQL_PASSWORD="zabbix_pwd" \
+      -e MYSQL_PASSWORD="$mysql_password" \
       -e PHP_TZ="America/Argentina/Buenos_Aires" \
       --network zabbix-net \
       --link mysql-server:mysql \
@@ -61,7 +69,7 @@ docker run --name zabbix-web -t \
 
 echo "Creating GRAFANA container"	  
 docker run \
---name grafana -d --hostname grafana.gustavoortega.com.ar \
+--name grafana -d --hostname grafana.avanti.la \
 -v grafana-config:/etc/grafana \
 -v grafana-storage:/var/lib/grafana/ \
 -p 3000:3000 \
@@ -75,3 +83,18 @@ docker network connect zabbix-net grafana
 ###AUX
 echo "Installing grafana plugin for zabbix"	  
 docker exec -ti grafana grafana-cli plugins install alexanderzobnin-zabbix-app	  
+
+
+docker run --name prometheus \
+--name prometheus -d --hostname prometheus.avanti.la \
+-p 9090:9090 \
+-v prometheus-storage:/prometheus \
+-v /var/lib/docker/volumes/prometheus-config/_data/prometheus.yml:/etc/prometheus/prometheus.yml \
+--network grafana-net \
+--restart unless-stopped \
+prom/prometheus
+
+docker run -d --name oidc \
+-p 8200:80 \
+--restart unless-stopped \
+local:oidc
